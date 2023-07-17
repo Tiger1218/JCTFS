c0fc7d6ad835c1e8e62b025dcc19496ab0d4fee3
Tiger1218 <tiger1218@foxmail.com>
2023-07-17 11:26:54 +0800

    fix: fix last commit's bugs ocurred in merging

--- JCTFS/src/main/java/com/ddteam/JCTFS/controller/Controller.java
+++ JCTFS/src/main/java/com/ddteam/JCTFS/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.ddteam.JCTFS.controller;
 
+import java.util.concurrent.ExecutionException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSON;
 import com.ddteam.JCTFS.db.ProblemManage;
 import com.ddteam.JCTFS.db.RecordsManage;
+import com.ddteam.JCTFS.db.UserManage;
 import com.ddteam.JCTFS.models.Problem;
 import com.ddteam.JCTFS.models.User;
 import com.ddteam.JCTFS.models.Record;
+import com.ddteam.JCTFS.models.Result;
 
 @RestController
 public class Controller {
@@ -23,6 +27,8 @@ public class Controller {
     private ProblemManage problemManage;
     @Autowired
     private RecordsManage recordsManage;
+    @Autowired
+    private UserManage userManage;
     @GetMapping("/devtest")
     public String devtest(){
         return "Hello, world";
@@ -47,5 +53,55 @@ public class Controller {
         Record new_record = new Record(problemID, User.token_to_id(token), flag);
         return ResponseEntity.ok(JSON.toJSONString(recordsManage.submit(new_record)));
         // return ResponseEntity.ok(new_object.toString());
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Result> login(String username, String hashed_passwd, String salt){
+        try {
+            User user=new User();
+            user.username=username;
+            user.hashed_passwd=hashed_passwd;
+            user.salt=salt;
+            Result result;
+            if(userManage.select(user)==null){
+                result=new Result(1,"用户名或密码错误","");
+            }
+            result=new Result<String>(0,"登录成功",user.signToken());
+            return ResponseEntity<result>;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch(Exception e){
+            
+        }
+        Result result=new Result(1,"登录失败","");
+        return ResponseEntity.ok(result);
+    }
+    ResponseEntity<Result> register(String username, String email, String hashed_passwd, String salt){
+        try {
+            User user=new User();
+            user.username=username;
+            
+            Result<String> result;
+            if(userManage.select(user)==null){
+                user.email=email;
+                user.hashed_passwd=hashed_passwd;
+                user.salt=salt;
+                user.ID=userManage.count()+1;
+                result=new Result(0,"注册成功",user.signToken());
+                userManage.add(user);
+            }else{
+                result=new Result(1,"用户已存在",user.signToken());
+            }
+            return ResponseEntity.ok(result);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch(Exception e){
+            
+        }
+        Result result=new Result(1,"注册失败","");
+        return ResponseEntity.ok(result);
     }
 }
--- JCTFS/src/main/java/com/ddteam/JCTFS/models/User.java
+++ JCTFS/src/main/java/com/ddteam/JCTFS/models/User.java
@@ -3,18 +3,23 @@ import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 
 import com.ddteam.JCTFS.models.Result;
-import com.ddteam.JCTFS.models.SolveRecord;
+import com.ddteam.JCTFS.models.Record;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.JWT;
+
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 public class User {
-	int ID;
+	public int ID;
 	boolean isAdmin;
-	int TeamID; // ID of team he belongs to.
-	SolveRecord[] records; // ID of problems he solved;
-	String nickname; 
-	String username;
-	String hashed_passwd, salt; // password which have been hashed by salt; (hex form)
-	String email;
+	public int TeamID; // ID of team he belongs to.
+	Record[] records; // ID of problems he solved;
+	public String nickname; 
+	public String username;
+	public String hashed_passwd, salt; // password which have been hashed by salt; (hex form)
+	public String email;
 	ResponseEntity<Result> register(String username, String email, String hashed_passwd, String salt){
         return ResponseEntity.status(200).body(null);
     }
@@ -24,11 +29,10 @@ public class User {
 
 	
 	private static final long TOKEN_EXP = 24 * 60 * 60 * 1000;//过期时间
-	public String signToken(){
+	public String signToken() throws IllegalArgumentException, JWTCreationException, UnsupportedEncodingException{
         Date date = new Date(System.currentTimeMillis() + TOKEN_EXP);
-        Algorithm algorithm = Algorithm.HMAC256(hashed_passwd);
         //附带username信息的token
-        return JWT.create().withClaim("ID",ID).withExpiresAt(date).sign(algorithm);
+        return JWT.create().withClaim("ID",ID).withExpiresAt(date).sign(Algorithm.HMAC256(hashed_passwd));
     }
 	User userview(){
 		User res=this;
