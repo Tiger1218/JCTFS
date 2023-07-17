61b2c591f20ea08c5d3509453e1b5531ac543807
Tiger1218 <tiger1218@foxmail.com>
2023-07-17 11:01:01 +0800

    feat: add user interfaces

--- JCTFS/src/main/java/com/ddteam/JCTFS/models/User.java
+++ JCTFS/src/main/java/com/ddteam/JCTFS/models/User.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 
 import com.ddteam.JCTFS.models.Result;
 import com.ddteam.JCTFS.models.SolveRecord;
+import java.util.Date;
 
 public class User {
 	int ID;
@@ -20,6 +21,20 @@ public class User {
 	ResponseEntity<Result> login(String username, String hashed_passwd, String salt){
         return ResponseEntity.status(200).body(null);
     }
+
+	
+	private static final long TOKEN_EXP = 24 * 60 * 60 * 1000;//过期时间
+	public String signToken(){
+        Date date = new Date(System.currentTimeMillis() + TOKEN_EXP);
+        Algorithm algorithm = Algorithm.HMAC256(hashed_passwd);
+        //附带username信息的token
+        return JWT.create().withClaim("ID",ID).withExpiresAt(date).sign(algorithm);
+    }
+	User userview(){
+		User res=this;
+		res.hashed_passwd=res.salt="";
+		return res;
+	}
 	// Options
 	// boolean hidden, banned;
 	// String affiliation;
